26199485f92dcc229d5aa1fa8990d3e612fc87ca
Aditya Prakash <aditya.prakash132@gmail.com>
2016-12-30 18:26:23 +0530

    Add order and sort options

--- lib/contribute/finder.rb
+++ lib/contribute/finder.rb
@@ -1,8 +1,12 @@
 module Contribute
   class Finder
-    def initialize(query = '')
+    SORT_OPTIONS = %w(created updated stars forks).freeze
+    ORDER_OPTIONS = %w(desc asc).freeze
+
+    def initialize(query = '', options = {})
       @finder_client = Contribute::Client.new.octokit
       @query = query
+      @options = options
     end
 
     def language(name)
@@ -20,13 +24,29 @@ module Contribute
       self
     end
 
+    def sort(field)
+      raise error_msg('sort', SORT_OPTIONS) unless SORT_OPTIONS.include? field
+      options[:sort] = field
+      self
+    end
+
+    def order(type)
+      raise error_msg('order', ORDER_OPTIONS) unless ORDER_OPTIONS.include? type
+      options[:order] = type
+      self
+    end
+
     def find
-      results = finder_client.search_repositories query
+      results = finder_client.search_repositories query, options
       puts results['items'].map { |r| r['full_name'] }
     end
 
   private
 
-    attr_reader :finder_client, :query
+    attr_reader :finder_client, :query, :options
+
+    def error_msg(type, valid_otions)
+      "Input value for '#{type}' was not valid. Valid inputs: #{valid_otions.join(', ')}"
+    end
   end
 end
